28ec16bf18922d321997229df3a85f1160cba00d
withColinSong <withColinSong@gmail.com>
2021-11-29 17:31:11 +0900

    add: DI

--- inflearn/inflearn-spring-yh/prj/hello-spring/src/test/java/hello/hellospring/service/MemberServiceTest.java
+++ inflearn/inflearn-spring-yh/prj/hello-spring/src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -5,6 +5,7 @@ import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -14,13 +15,20 @@ import static org.junit.jupiter.api.Assertions.fail;
 
 class MemberServiceTest {
 
-    MemberService memberService = new MemberService();
-    MemoryMemberRepository memberRepository = new MemoryMemberRepository();
+    MemberService memberService;
+    MemoryMemberRepository memberRepository;
 
-//    @AfterEach
-//    public void afterEach() {
-//        memberRepository.clear();
-//    }
+    // DI: 외부에서 생성 시킬 때
+    @BeforeEach
+    public void beforeEach() {
+        memberRepository = new MemoryMemberRepository();
+        memberService = new MemberService(memberRepository);
+    }
+
+    @AfterEach
+    public void afterEach() {
+        memberRepository.clear();
+    }
 
 
     @Test
