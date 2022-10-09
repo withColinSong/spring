47f668ef9cc06d0f89da6e7c0e164e27167a1e03
withColinSong <withColinSong@gmail.com>
2022-10-09 19:47:47 +0900

    add : bean 등록

--- properties/properties/src/main/java/org/com/properties/config/SingerConfig.java
+++ properties/properties/src/main/java/org/com/properties/config/SingerConfig.java
@@ -5,8 +5,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
 @Slf4j
+@Configuration
 @EnableConfigurationProperties({SingerProperties.class})
 @RequiredArgsConstructor
 public class SingerConfig implements ApplicationRunner {
