44588432c887e7fb9d6564116c0e1386887dd1ad
withColinSong <withColinSong@gmail.com>
2023-02-10 14:35:39 +0900

    add: web socket

---
+++ webSocket/socket/src/main/java/com/web/socket/controller/SocketController.java
@@ -0,0 +1,19 @@
+package com.web.socket.controller;
+
+import com.web.socket.entity.Greeting;
+import com.web.socket.entity.HelloMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+
+@Controller
+public class SocketController {
+
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public Greeting greeting(HelloMessage message) throws InterruptedException {
+        Thread.sleep(1000);
+        return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+    }
+}
