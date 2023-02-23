4e35388e22128b3bf84abd50bef3407fb4d61846
withColinSong <withColinSong@gmail.com>
2023-02-23 22:06:14 +0900

    add: jwt token controller

--- jwt/jwt/src/main/java/com/song/jwt/controller/MainController.java
+++ jwt/jwt/src/main/java/com/song/jwt/controller/MainController.java
@@ -1,13 +1,32 @@
 package com.song.jwt.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
+@Slf4j
 public class MainController {
+
     @GetMapping("/health")
     public ResponseEntity<?> hello() {
-        return ResponseEntity.ok("hello");
+        return ResponseEntity.ok().build();
     }
+
+    @GetMapping("/")
+    public ResponseEntity<?> auth(Authentication authentication,
+                                  @RequestHeader Map<String, String> headers,
+                                  @RequestHeader("user-agent") String userAgent) {
+
+        for (Map.Entry<String, String> entry : headers.entrySet()) {
+           log.info("key: " + entry.getKey() + ", value: " + entry.getValue());
+        }
+        return ResponseEntity.ok(authentication.getName() + "!");
+    }
+
 }
