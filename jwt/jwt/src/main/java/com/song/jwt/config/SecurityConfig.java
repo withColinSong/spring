2149d849dbf0ef32df0088d0e99d00d25b351836
withColinSong <withColinSong@gmail.com>
2023-02-23 22:05:29 +0900

    add : security test users

--- jwt/jwt/src/main/java/com/song/jwt/config/SecurityConfig.java
+++ jwt/jwt/src/main/java/com/song/jwt/config/SecurityConfig.java
@@ -2,8 +2,15 @@ package com.song.jwt.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.server.resource.web.BearerTokenAuthenticationEntryPoint;
+import org.springframework.security.oauth2.server.resource.web.access.BearerTokenAccessDeniedHandler;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,12 +20,30 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
 
-        return httpSecurity
+        httpSecurity
                 .csrf().disable()
                 .authorizeHttpRequests()
                     .requestMatchers("/health").permitAll()
                     .anyRequest().authenticated()
+
                 .and()
-                .build();
+                .httpBasic(Customizer.withDefaults())
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
+                .exceptionHandling((exceptions) -> exceptions
+                        .authenticationEntryPoint(new BearerTokenAuthenticationEntryPoint())
+                        .accessDeniedHandler(new BearerTokenAccessDeniedHandler())
+                );
+
+                return httpSecurity.build();
+    }
+
+    @Bean
+    UserDetailsService users() {
+        return new InMemoryUserDetailsManager(
+                User.withUsername("song")
+                        .password("{noop}password") // {noop} : Spring Security에서 텍스트 그대로 비밀번호로 인식
+                        .authorities("app")
+                        .build()
+        );
     }
 }
