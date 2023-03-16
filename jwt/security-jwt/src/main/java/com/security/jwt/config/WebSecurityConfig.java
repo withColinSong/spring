dfb9eb26f7a94e49dbf31e41f42ae56e44d5e863
withColinSong <withColinSong@gmail.com>
2023-03-16 23:42:06 +0900

    add : jwt public/private key yml

--- jwt/security-jwt/src/main/java/com/security/jwt/config/WebSecurityConfig.java
+++ jwt/security-jwt/src/main/java/com/security/jwt/config/WebSecurityConfig.java
@@ -1,29 +1,18 @@
 package com.security.jwt.config;
 
 
-import com.security.jwt.service.AccountService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.stereotype.Component;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
-
-    private final AccountService accountService;
-
-    public WebSecurityConfig(AccountService accountService) {
-        this.accountService = accountService;
-    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -36,9 +25,11 @@ public class WebSecurityConfig {
                         (req) -> req.requestMatchers("/", "/login").permitAll()
                                 .anyRequest().authenticated()
                 )
-                .userDetailsService(accountService)
+                .sessionManagement().disable()
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
                 .httpBasic(withDefaults())
                 .build();
     }
+
 
 }
