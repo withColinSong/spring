4c7a63a075f40c414d4af394646fc3bb2147c142
withColinSong <withColinSong@gmail.com>
2023-03-17 15:42:39 +0900

    add: security config jwt Authentication converter

--- jwt/security-jwt/src/main/java/com/security/jwt/config/WebSecurityConfig.java
+++ jwt/security-jwt/src/main/java/com/security/jwt/config/WebSecurityConfig.java
@@ -1,17 +1,22 @@
 package com.security.jwt.config;
 
-
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
+
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
+@Slf4j
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class WebSecurityConfig {
 
     @Bean
@@ -22,14 +27,36 @@ public class WebSecurityConfig {
                 .cors()
                 .and().formLogin().disable()
                 .authorizeHttpRequests(
-                        (req) -> req.requestMatchers("/", "/login").permitAll()
+                        (req) -> req.requestMatchers("/", "/login")
+                                .permitAll()
                                 .anyRequest().authenticated()
                 )
                 .sessionManagement().disable()
-                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
+                //oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt
+                                .jwtAuthenticationConverter(jwtAuthenticationConverter())
+                        )
+                )
                 .httpBasic(withDefaults())
                 .build();
     }
+
+    /**
+     *
+     *  jwt getAuthorities 인가
+     *  @setAuthorityPrefix : SCOPE_로 시작됨 hasRole은 default prifix를 서로 맞춰줘야함.
+     */
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtGrantedAuthoritiesConverter grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+
+        grantedAuthoritiesConverter.setAuthorityPrefix("ROLE_");
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(grantedAuthoritiesConverter);
+        return jwtAuthenticationConverter;
+    }
+
 
 
 }
