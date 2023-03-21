ea726953c293a7bc3693b7327f4f2e3734901530
withColinSong <withColinSong@gmail.com>
2023-03-21 22:30:00 +0900

    fix : jwt remove userDetails

--- jwt/security-jwt/src/main/java/com/security/jwt/entity/User.java
+++ jwt/security-jwt/src/main/java/com/security/jwt/entity/User.java
@@ -4,20 +4,11 @@ import com.security.jwt.enums.RoleType;
 import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Getter
 @Entity
 @Table(name = "USERS")
-public class User implements UserDetails {
+public class User  {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -29,43 +20,6 @@ public class User implements UserDetails {
 
     @Enumerated(value = EnumType.STRING)
     private RoleType roleType;
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        Collection<GrantedAuthority> collect = new ArrayList<>();
-        collect.add(new GrantedAuthority() {
-            @Override
-            public String getAuthority() {
-                return roleType.toString();
-            }
-        });
-        return null;
-    }
-
-    @Override
-    public String getUsername() {
-        return name;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return false;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return false;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return false;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return false;
-    }
 
     public User() { }
 
